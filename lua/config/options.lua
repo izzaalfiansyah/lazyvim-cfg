9787dd939fc31220b4648efa5419a1559287fd2c
izzaalfiansyah <iansyah724@gmail.com>
2025-07-03 18:02:19 +0700

    add options to global wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
 vim.g.lazyvim_php_lsp = "phpactor"
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+opt.wrap = true
