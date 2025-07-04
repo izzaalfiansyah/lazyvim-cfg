b3c7a3fc893c91047d9a23e77d81e1050e9c5285
izzaalfiansyah <iansyah724@gmail.com>
2025-07-04 16:16:28 +0700

    update keymaps for change tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,30 +1,32 @@
-local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
--- ~/.config/nvim/lua/config/keymaps.lua
-keymap.set("n", "<leader>ff", function()
-  ("telescope.builtin").find_files({
+
+-- Ignore dotfiles on telescope
+vim.keymap.set("n", "<leader>ff", function()
+  require("telescope.builtin").find_files({
     hidden = true,
     no_ignore = true,
   })
 end, { desc = "Find files (+dotfiles, +ignored)" })
 
-keymap.set("n", "<leader><space>", function()
-  ("telescope.builtin").find_files({
+vim.keymap.set("n", "<leader><space>", function()
+  require("telescope.builtin").find_files({
     hidden = true,
     no_ignore = true,
   })
 end, { desc = "Find files (include dotfiles & ignored)" })
 
-keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
--- keymap.set("v", "jk", "<Esc>", { desc = "Exit visual mode with jk" })
-keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode with jk", noremap = true, silent = true })
-keymap.set("i", "..", "->", { desc = "Type -> in insert mode", noremap = true })
-keymap.set("i", ";;", "=>", { desc = "Type => in insert mode", noremap = true })
--- keymap.set("n", "<C-j>", "<C-d>", { desc = "Scroll down half a page", noremap = true })
--- keymap.set("n", "<C-k>", "<C-u>", { desc = "Scroll up half a page", noremap = true })
+-- Exit insert, visual, and terminal mode using jk
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("v", "jk", "<Esc>", { desc = "Exit visual mode with jk" })
+vim.keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode with jk", noremap = true, silent = true })
 
-keymap.set("n", "<C-a>", "gg<S-v>G")
+-- Keymap for set attribute from object and set array on php
+vim.keymap.set("i", "..", "->", { desc = "Type -> in insert mode", noremap = true })
+vim.keymap.set("i", "::", "=>", { desc = "Type => in insert mode", noremap = true })
 
--- Jumplist
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprevious<Return>", opts)
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-a>G")
+
+-- Jump tab
+vim.keymap.set("n", "<tab>", "]b", opts)
+vim.keymap.set("n", "<S-tab>", "[b", opts)
