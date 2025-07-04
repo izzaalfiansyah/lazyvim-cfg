c9662eeabdc12d66b43a03d0ed211312a1683d0c
izzaalfiansyah <iansyah724@gmail.com>
2025-07-04 16:04:27 +0700

    add keymap to change tab

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,22 +1,30 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 -- ~/.config/nvim/lua/config/keymaps.lua
-vim.keymap.set("n", "<leader>ff", function()
-  require("telescope.builtin").find_files({
+keymap.set("n", "<leader>ff", function()
+  ("telescope.builtin").find_files({
     hidden = true,
     no_ignore = true,
   })
 end, { desc = "Find files (+dotfiles, +ignored)" })
 
-vim.keymap.set("n", "<leader><space>", function()
-  require("telescope.builtin").find_files({
+keymap.set("n", "<leader><space>", function()
+  ("telescope.builtin").find_files({
     hidden = true,
     no_ignore = true,
   })
 end, { desc = "Find files (include dotfiles & ignored)" })
 
-vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
--- vim.keymap.set("v", "jk", "<Esc>", { desc = "Exit visual mode with jk" })
-vim.keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode with jk", noremap = true, silent = true })
-vim.keymap.set("i", "..", "->", { desc = "Type -> in insert mode", noremap = true })
-vim.keymap.set("i", ";;", "=>", { desc = "Type => in insert mode", noremap = true })
--- vim.keymap.set("n", "<C-j>", "<C-d>", { desc = "Scroll down half a page", noremap = true })
--- vim.keymap.set("n", "<C-k>", "<C-u>", { desc = "Scroll up half a page", noremap = true })
+keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+-- keymap.set("v", "jk", "<Esc>", { desc = "Exit visual mode with jk" })
+keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode with jk", noremap = true, silent = true })
+keymap.set("i", "..", "->", { desc = "Type -> in insert mode", noremap = true })
+keymap.set("i", ";;", "=>", { desc = "Type => in insert mode", noremap = true })
+-- keymap.set("n", "<C-j>", "<C-d>", { desc = "Scroll down half a page", noremap = true })
+-- keymap.set("n", "<C-k>", "<C-u>", { desc = "Scroll up half a page", noremap = true })
+
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprevious<Return>", opts)
